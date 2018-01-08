26980c4be73c9a203d8a7e479aa420ddc6bbb670
Omar Atia <atiaomar1978@gmail.com>
2018-01-08 10:13:52 +0200

    Add files via upload

---
+++ TryApp/TryApp/Controllers/TodoController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TryApp.Models;
+using System.Linq;
+
+namespace TryApp.Controllers
+{
+    [Route("api/rest")]
+    public class TodoController : Controller
+    {
+        //      private readonly TodoContext _context;
+        /*     [HttpGet]
+           public IEnumerable<TodoItem> GetAll()
+            {
+                return _context.TodoItems.ToList();
+            }
+
+            [HttpGet("{id}", Name = "GetTodo")]
+            public IActionResult GetById(long id)
+            {
+                var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                return new ObjectResult(item);
+            }
+            */
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            PlayItem test = new PlayItem();
+            test.Id = 0;
+            test.game = "PS4";
+            test.Name = "OmarAtia";
+            test.IsComplete = false;
+            //    MusicStoreContext trial= new MusicStoreContext("server=localhost;Password=sasql;Persist Security Info=True;User ID=sa;Initial Catalog=test;Data Source=.");
+
+
+            MusicStoreContext context = HttpContext.RequestServices.GetService(typeof(TryApp.Models.MusicStoreContext)) as MusicStoreContext;
+
+           string Item= context.GetAllAlbums().FirstOrDefault().Item.Trim();
+            string Name= context.GetAllAlbums().FirstOrDefault().Name.Trim();
+
+            Album trial = new Album();
+            trial.Item = Item;
+            trial.Name = Name;
+            trial.Id = context.GetAllAlbums().FirstOrDefault().Id;
+
+
+            return new ObjectResult(trial);
+
+
+          //  MusicStoreContext trial= new MusicStoreContext();
+
+         //   Album raq = trial.GetAllAlbums().FirstOrDefault();
+          //  _context.TodoItems.Add(item);
+
+            //   _context.TodoItems.Add(FirstName);
+            //   _context.TodoItems.Add(LastName);
+
+            // _context.SaveChanges();
+
+            //  return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+            //  return new ObjectResult(test);
+          //  return new ObjectResult(raq);
+
+        }
+
+       
+
+
+
+
+      /*  [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+public IActionResult Delete(long id)
+{
+    var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+    if (todo == null)
+    {
+        return NotFound();
+    }
+
+    _context.TodoItems.Remove(todo);
+    _context.SaveChanges();
+    return new NoContentResult();
+}
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }*/
+    }
+}
---
+++ TryApp/TryApp/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TryApp.Models;
+
+
+namespace TryApp
+{
+    public class Startup
+    {
+        //  public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
+
+        {
+           // Configuration = configuration;
+           var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+              .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //   services.AddDbContext<TryApp.Models.TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            //Data Source=OMARATIA;Initial Catalog=master;Integrated Security=True
+         //    services.AddDbContext<TryApp.Models.MusicStoreContext>(opt => opt.UseSqlServer("Password = sasql; Persist Security Info = True; User ID = sasql; Initial Catalog = test; Data Source =."));
+         //  services.Add(new ServiceDescriptor(typeof(MusicStoreContext), new MusicStoreContext("Password=sasql;Persist Security Info=True;User ID=sasql;Initial Catalog=test;Data Source=.")));
+
+            services.AddMvc();
+            services.Add(new ServiceDescriptor(typeof(MusicStoreContext), new MusicStoreContext(Configuration.GetConnectionString("DefaultConnection"))));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
