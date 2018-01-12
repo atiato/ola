27784372fc012c26bbfe151ab454579675d7a2d8
Omar Atia <atiaomar1978@gmail.com>
2018-01-12 12:26:52 +0200

    Add files via upload

---
+++ latestwebauth/People365.WebAPIAuth/Controllers/TokenController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIAuth.Models;
+
+namespace WebAPIAuth.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Post([FromBody]LoginModel loginViewModel)
+        {
+         //   int userid = GetUserIdFromCredentials(loginViewModel);
+        /*    if(userid == -1)
+            {
+                return Unauthorized();
+            }*/
+
+            JwtSecurityToken token = new JwtSecurityToken
+               (
+                   issuer: "MiraRND",//_configuration["Issuer"],
+                   audience: "MiraRND", //_configuration["Audience"],
+                   claims: null,
+                   expires: DateTime.UtcNow.AddDays(60),
+                   notBefore: DateTime.UtcNow,
+                   signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("System.ArgumentOutOfRangeException")),
+                        SecurityAlgorithms.HmacSha256)
+               );
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        private int GetUserIdFromCredentials(LoginModel loginViewModel)
+        {
+            var userId = -1;
+            if (loginViewModel.Username == "mira" && loginViewModel.Password == "P@ssw0rd")
+            {
+                userId = 5;
+            }
+
+            return userId;
+        }
+    }
+}
---
+++ latestwebauth/People365.WebAPIAuth/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebAPIAuth
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+
+        {
+            // Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+               .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(jwtBearerOptions =>
+            {
+                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateActor = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Issuer"],
+                    ValidAudience = Configuration["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SigningKey"]))
+                };
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
