fcd254ccaf0624bf92743f8d6aec9a82dc7d2b4a
Omar Atia <atiaomar1978@gmail.com>
2018-01-12 21:24:03 +0200

    Add files via upload
    Added Redis Support

---
+++ RedisTest/Models/PlayItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TryApp.Models
+{
+    public class PlayItem
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string game { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
