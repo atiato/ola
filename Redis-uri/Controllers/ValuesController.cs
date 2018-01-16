9888408b7fb97ac113dd8c0e334a964920c4f2e0
Omar Atia <atiaomar1978@gmail.com>
2018-01-16 22:32:35 +0200

    Add files via upload

---
+++ Redis-uri/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace TryApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+
+        private readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get(string key)
+        {
+            // var cacheKey = "TheTime";
+            var cacheKey = key;
+            var existingTime = _distributedCache.GetString(cacheKey);
+            if (!string.IsNullOrEmpty(existingTime))
+            {
+                return new string[] { "Fetched from cache : " + existingTime };
+            }
+            else
+            {
+                existingTime = DateTime.UtcNow.ToString();
+                _distributedCache.SetString(cacheKey, existingTime);
+                return new string[] { "Added to cache : " + existingTime };
+            }
+        }
+        // GET api/values
+    /*    [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }*/
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
