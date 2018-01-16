b55643964e5994f9f35345bf92f43fe2502b4902
Omar Atia <atiaomar1978@gmail.com>
2018-01-16 23:05:03 +0200

    Add files via upload

---
+++ redis-query/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿namespace TryApp.Models
+{
+    public class Store
+    {
+        private StoreContext trial;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Item { get; set; }
+
+        public string NoOfDays { get; set; }
+
+
+
+    }
+}
---
+++ redis-query/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TryApp.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
