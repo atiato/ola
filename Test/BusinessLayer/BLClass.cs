d4bbb99e6f7f5d429a235867fbf3a48359581a40
Omar Atia <atiaomar1978@gmail.com>
2018-02-02 16:31:51 +0200

    Add files via upload

---
+++ Test/BusinessLayer/BLClass.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class BLClass
+    {
+        public List<string> TestBL()
+        {
+            DAClass dAClass = new DAClass();
+            return dAClass.TestDa();
+        }
+    }
+}
---
+++ Test/DataLayer/DAClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer
+{
+    public class DAClass
+    {
+        public List<string> TestDa()
+        {
+            return new List<string> { "String1", "String2" };
+        }
+    }
+}
